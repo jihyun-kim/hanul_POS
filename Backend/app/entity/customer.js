f7718c30d9b9a90b3703bef2d41f99c0569217e9
jihyun-kim <jihyun1058@gmail.com>
2021-11-21 15:17:12 +0900

    2021-11-21 update2

--- Backend/app/entity/customer.js
+++ Backend/app/entity/customer.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Customer = /** @class */ (function () {
-    function Customer(id, name, address, salary) {
+    function Customer(id, name, chName, gender, birthDate, address, telNo, etc) {
         this.id = id;
         this.name = name;
+        this.chName = chName;
+        this.gender = gender;
+        this.birthDate = birthDate;
         this.address = address;
-        this.salary = salary;
+        this.telNo = telNo;
+        this.etc = etc;
     }
     return Customer;
 }());
